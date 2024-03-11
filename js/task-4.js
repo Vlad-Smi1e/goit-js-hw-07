0ffd66976092d8d6bfb80624db0d9adae4b4b9ae
Vlad-Smi1e <mixirevanton@gmail.com>
2024-03-11 09:49:38 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,22 +1,22 @@
-const form = document.querySelector('.login-form');
 
-form.addEventListener('submit', event => {
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', event => {
   event.preventDefault();
 
-  const email = form.elements['email'].value.trim();
-  const password = form.elements['password'].value.trim();
+  const email = loginForm.elements.email.value.trim();
+  const password = loginForm.elements.password.value.trim();
 
   if (!email || !password) {
     alert('All form fields must be filled in');
     return;
   }
 
-  const form = {
-    email: email,
-    password: password,
+  const formData = {
+    email,
+    password,
   };
 
-  console.log(form);
-
-  form.reset();
+  console.log(formData);
+  loginForm.reset();
 });
